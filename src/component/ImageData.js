a05bda127dbe145ce40c08580387033f7de7dad4
Jetsadakorn <my1471@hotmail.com>
2023-02-08 15:20:57 +0700

    finish
    show data by using state to change data

---
+++ src/component/ImageData.js
@@ -0,0 +1,11 @@
+const ImageData =[
+    {
+        title:"ชายหาด",
+        img:"https://cdn.pixabay.com/photo/2016/11/14/22/18/beach-1824855_960_720.jpg"
+    },
+    {
+        title:"ภูเขา",
+        img:"https://cdn.pixabay.com/photo/2013/10/02/23/03/mountains-190055_960_720.jpg"
+    }
+]
+export default ImageData
